42b19e2bcc15413b6823129aa887b23df8a44eb1
ktrujillo16 <kedyn1005@gmail.com>
2020-05-25 11:30:10 -0500

    Mi primer commit

---
+++ src/main/java/com/example/demo/ProyectoDawiT6BnGrupo4Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDawiT6BnGrupo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDawiT6BnGrupo4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/ProyectoDawiT6BnGrupo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDawiT6BnGrupo4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
